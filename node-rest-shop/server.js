183f95870590432934171f0bc428bf37a58adb15
Quang Nguyen Huynh <92911952+nguyenhuynhquang2909@users.noreply.github.com>
2024-03-14 15:30:42 +0700

    Delete node-rest-shop directory

--- node-rest-shop/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app')
-const port = process.env.PORT || 8080;
-
-const server = http.createServer(app);
-
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-  });
